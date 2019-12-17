0a28af1725eb5f0ed1068fe0273a64b7b8ab09c3
Nandhini-Krishna <nandhinikrishna0615@gmail.com>
2019-12-17 10:50:38 +0530

    B

--- Card (copy).js
+++ Card (copy).js
@@ -12,5 +12,17 @@ const Card = (props) => {
 	</div>
 	);
 }
+const Card = (props) => {
+	const {name ,email ,id} = props;
+	return (
+	<div className='tc bg-washed-red dib br3 pa2 ma2 grow bw2 shadow-5 sample'>
+	<img alt='robo' src={`https://robohash.org/${id}?20*20`}/>
+	<div>
+	<h2>{name}</h2>
+	<p>{email}</p>
+	</div>
+	</div>
+	);
+}
 
 export default Card;
